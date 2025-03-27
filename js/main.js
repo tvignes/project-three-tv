1171ea3bb826d76b980be2cd9270f366ee383415
tvignes <tcvignes1@gmail.com>
2025-03-27 10:57:21 -0500

    initial commit with clipping mask example

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
 // JS scripts placed here
+var outerContainer = document.getElementById('outer-container');
+
+function getPosition(event) {
+	var top = event.clientY;
+	var left = event.clientX;
+	var size = "circle(0 at";
+	var clipStyle = size + " " + left + "px " + top + "px)";
+	event.target.nextElementSibling.style.transition = "0s";
+	event.target.nextElementSibling.style.webkitClipPath = clipStyle;
+	event.target.nextElementSibling.style.clipPath = clipStyle;
+}
+
+function show(event) {
+	var top = event.clientY;
+	var left = event.clientX;
+	var size = "circle(150% at";
+	var clipStyle = size + " " + left + "px " + top + "px)";
+	event.target.nextElementSibling.style.transition = "2s ease-out";
+	event.target.nextElementSibling.style.webkitClipPath = clipStyle;
+	event.target.nextElementSibling.style.clipPath = clipStyle;
+}
+
+function preventMouseOut(event) {	
+	event.target.previousElementSibling.removeEventListener('mousemove', getPosition);
+}
+
+for (var i = 0; i < outerContainer.children.length; i++) {
+	outerContainer.children[i].addEventListener('click', show);
+	outerContainer.children[i].addEventListener('mousemove', getPosition);
+	outerContainer.children[i].addEventListener('mouseout', preventMouseOut);
+}
